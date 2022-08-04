aa77329f4575061642c72af5f3d8cafefbd38ffb
gev_org <thecarguyarm@gmail.com>
2022-08-04 23:44:28 +0400

    hierarchy and module added

---
+++ Assets/variables.js
@@ -0,0 +1,7 @@
+let cardIsOpen = false;
+const rootElement = document.getElementById('root');
+const contentElement = document.getElementById('content');
+let input = document.getElementById('input').value;
+let value = input.value;
+
+export {cardIsOpen,rootElement,contentElement,input,value};
